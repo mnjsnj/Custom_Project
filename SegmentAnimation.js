afe1808f0829f9d55f0d0425d5abeafa446e29a1
Manoj Bhardwaj <19218349+mnjsnj@users.noreply.github.com>
2021-06-04 09:34:27 +0530

    Update SegmentAnimation.js

--- SegmentAnimation.js
+++ SegmentAnimation.js
@@ -1,66 +1,144 @@
-import React,{useRef,useEffect,useState} from 'react';
+import React,{useRef,useEffect,useState} from 'react'
+import { View,Dimensions,ScrollView} from 'react-native'
 import styled from 'styled-components/native';
 import LottieView from 'lottie-react-native';
+const ScrollViewer = () => {
+    const [sliderState, setSliderState] = useState({ currentPage: 0 });
+    const { width, height } = Dimensions.get('window');
 
-const SegmentAnimation = ({SourcePath,StartSegMent,EndSegment}) => {
-
-  const [Loop, setLoop] = useState(false)
-  const animation = useRef(null);
- 
-  const Start=()=>{
-    setLoop(false) 
-    animation.current.play(StartSegMent.First,StartSegMent.Second);
-  }
-
-   useEffect(() => { 
-       Start();
-   }, []) 
-
-   const Finish=()=>{   
-    setLoop(true)  
-    animation.current.play(EndSegment.First,EndSegment.Second)
-   } 
-  
-  //  1  0,198   and 225,350
-  
-  
-  return (
+    const [LoopAnimationone, setLoopAnimationone] = useState(false);
+    const [LoopAnimationtwo, setLoopAnimationtwo] = useState(false)
     
-    <SliderView> 
-        <LottieViewArea> 
-                <LottieView   
-                  ref={animation}
-                  loop={Loop}
-                  onAnimationFinish={()=>Finish()}
-                  source={SourcePath}
-               />  
-        </LottieViewArea> 
-      </SliderView> 
+    
+
+    const animationone = useRef(null);
+    const animationtwo = useRef(null);
+    const animationthree = useRef(null);
+
+    const setSliderPage = (event) => {
+      const { currentPage } = sliderState;
+      const { x } = event.nativeEvent.contentOffset;
+      const indexOfNextScreen = Math.floor(x / width);
+      if (indexOfNextScreen !== currentPage) {
+
+        setSliderState({
+          ...sliderState,
+          currentPage: indexOfNextScreen,
+        })
+
+        if(indexOfNextScreen===0){
+          animationtwo.current.reset();
+          animationthree.current.reset();
+          animationone.current.play(0,198);
+        }
+
+        if(indexOfNextScreen===1){
+          animationtwo.current.play(0,150);
+          animationone.current.reset();
+          animationthree.current.reset();
+        }
+
+        if(indexOfNextScreen===2){
+          animationthree.current.play(0,140);
+          animationone.current.reset();
+          animationtwo.current.reset();
+        }
+         
+      }
+    }
+
+     
+    
+    useEffect(() => {
+      animationone.current.play(0,198);
+      animationtwo.current.pause();
+      animationthree.current.pause();
+    }, [])
+
+    
+    const { currentPage } = sliderState;
+  
+    return (
+      <>
+       
+        <SliderView>
+          <ScrollView
+            style={{ flex: 1 }}
+            horizontal={true}
+            scrollEventThrottle={16}
+            pagingEnabled={true}
+            showsHorizontalScrollIndicator={false}
+            onScroll={(event) => {
+              setSliderPage(event);
+            }}
+          >
+            
+            <View style={{ width, height }}>
+            
+                <LottieViewArea> 
+                        <LottieView   
+                        ref={animationone}  
+                        loop={LoopAnimationone}
+                        onAnimationFinish={()=>{ 
+                          //  setLoopAnimationone(true)
+                          //  animationone.current.play(198,350);
+                        }} 
+                        source={require('../assest/1.json')}
+                    />   
+                </LottieViewArea> 
+           
+            </View>
+
+            <View style={{ width, height }}>
+          
+                <LottieViewArea> 
+                        <LottieView   
+                        ref={animationtwo} 
+                        loop={false}
+                        onAnimationFinish={()=>{
+                         
+                        }}
+                        source={require('../assest/2.json')}
+                    />   
+                </LottieViewArea> 
+         
+            </View>
+
+
+            <View style={{ width, height }}>
+          
+          <LottieViewArea> 
+                  <LottieView   
+                  ref={animationthree} 
+                  loop={false}
+                  onAnimationFinish={()=>{
+                   
+                  }}
+                  source={require('../assest/3.json')}
+              />   
+          </LottieViewArea> 
    
-  )
-}
+      </View>
 
 
-const SliderView=styled.SafeAreaView`
+            
+          </ScrollView>
+   
+        </SliderView>
+      </>
+    );
+  };
+
+const SliderView=styled.View`
        flex:1;
 `;
-const LottieViewArea=styled.View`
+const LottieViewArea=styled.View` 
    align-items:center;
    height:100%;
    width:100%;
    justify-content:center;
 `;
-export default SegmentAnimation;
 
-
-
-// Use And Import of comopnet example
-
-
-// <SegmentAnimation
-//        StartSegMent={{First:0,Second:198}} 
-//        EndSegment={{First:225,Second:350}} 
-//        SourcePath={require('./src/assest/1.json')}/>
-
+  export default ScrollViewer;
 
 
